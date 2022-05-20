af77681501b90de4bd3e15ffe37dd16c4e36a160
kaitoShy <i@kaitoshy.com>
2022-05-21 01:04:58 +0800

    提交代码

--- my-util/src/main/java/com/kaitoshy/utils/ZipUtil.java
+++ my-util/src/main/java/com/kaitoshy/utils/ZipUtil.java
@@ -1,22 +1,14 @@
 package com.kaitoshy.utils;
 
-import sun.misc.IOUtils;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Objects;
@@ -28,13 +20,13 @@ public class ZipUtil {
 
     /**
      * 将需要目录中的文件压缩成 zip
+     *
      * @param filePath 需要压缩的路径
-     * @param zipFile 压缩后的路径
+     * @param zipFile  压缩后的路径
      * @return
      */
     public static void zip(String filePath, String zipFile) throws IOException {
-        Path path = Paths.get(filePath);
-        File file = path.toFile();
+        File file = new File(filePath);
 
         // 如果路径不存在或者不是路径直接返回
         if (!file.exists() || !file.isDirectory()) {
@@ -42,32 +34,85 @@ public class ZipUtil {
             return;
         }
 
-        List<File> allFiles = new LinkedList<>();
+        List<File> allFiles = new ArrayList<>();
         getAllFile(file, allFiles);
-        FileOutputStream outputStream = new FileOutputStream(zipFile);
-        ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream);
-        for (File f: allFiles) {
-            byte[] buffer = new byte[2048];
-            String name = f.getAbsolutePath().substring(file.getAbsolutePath().length() + 1);
-            System.out.println(f.getAbsolutePath() + "=====" + name);
-            // 设置 entry 节点
-            ZipEntry zipEntry = new ZipEntry(name);
-            zipOutputStream.putNextEntry(zipEntry);
 
-            // 写入数据流
-            FileInputStream fileInputStream = new FileInputStream(f);
-            int read;
-            while ((read = fileInputStream.read(buffer, 0, buffer.length)) > 0) {
-                zipOutputStream.write(buffer, 0, read);
+        try (
+                // 文件输出流
+                FileOutputStream outputStream = new FileOutputStream(zipFile);
+                // zip 包输出流
+                ZipOutputStream zipOutputStream = new ZipOutputStream(outputStream)) {
+            for (File f : allFiles) {
+                byte[] buffer = new byte[2048];
+                String name = f.getAbsolutePath().substring(file.getAbsolutePath().length() + 1);
+                System.out.println(f.getAbsolutePath() + "=====" + name);
+                // 设置 entry 节点
+                ZipEntry zipEntry = new ZipEntry(name);
+                zipOutputStream.putNextEntry(zipEntry);
+
+                // 写入数据流
+                try (FileInputStream fileInputStream = new FileInputStream(f)) {
+                    int read;
+                    while ((read = fileInputStream.read(buffer, 0, buffer.length)) > 0) {
+                        zipOutputStream.write(buffer, 0, read);
+                    }
+                }
+
+                zipOutputStream.closeEntry();
             }
+        }
+    }
 
-            zipOutputStream.closeEntry();
-            fileInputStream.close();
+
+    public static void unzip(String zipFile, String decompressPath) throws IOException {
+        // 这里可以优化，目前只支持一层的外部路径
+        Path path = Paths.get(decompressPath);
+        File decompressPathFile = path.toFile();
+        if (!decompressPathFile.exists()) {
+            decompressPathFile.mkdirs();
+        }
+
+        try (
+        FileInputStream fileInputStream = new FileInputStream(zipFile);
+        ZipInputStream zipInputStream = new ZipInputStream(fileInputStream)) {
+
+            ZipEntry zipEntry;
+            byte[] buffer = new byte[2048];
+            while ((zipEntry = zipInputStream.getNextEntry()) != null) {
+                Path formatPath = path.resolve(zipEntry.getName());
+                if (zipEntry.getName().contains("/")) {
+                    mkdirAllDir(zipEntry.getName(), decompressPath);
+                }
+                try (
+                        // 文件输出流
+                        FileOutputStream fos = new FileOutputStream(formatPath.toFile());
+                        // 内容输出流
+                        BufferedOutputStream bos = new BufferedOutputStream(fos, buffer.length)) {
+                    int read;
+                    if ((read = zipInputStream.read(buffer, 0, buffer.length)) > 0) {
+                        bos.write(buffer, 0, read);
+                    }
+                }
+            }
+        }
+
+    }
+
+    private static void mkdirAllDir(String name, String decompressPath) {
+        String[] paths = name.split("/");
+        StringBuilder allPath = new StringBuilder(decompressPath + "/");
+        for (int i = 0; i < paths.length - 1; i++) {
+            allPath.append(paths[0]);
+            if (!new File(allPath.toString()).mkdirs()) {
+                System.out.println("创建文件失败！！");
+                return;
+            }
         }
     }
 
     /**
      * 递归获取对应的文件
+     *
      * @param file
      * @return
      */
@@ -75,7 +120,7 @@ public class ZipUtil {
         if (file.isDirectory()) {
             File[] files = file.listFiles();
             if (Objects.nonNull(files)) {
-                for (File f: files) {
+                for (File f : files) {
                     getAllFile(f, af);
                 }
             }
@@ -89,7 +134,8 @@ public class ZipUtil {
 
     public static void main(String[] args) {
         try {
-            zip("E:\\workspace\\tmp\\dir222", "E:\\workspace\\tmp\\a.zip");
+            zip("tmp/dir222", "tmp/b.zip");
+            unzip("tmp/b.zip", "tmp/dddd444");
         } catch (IOException ioException) {
             ioException.printStackTrace();
         }
